e64218751480233718c4cc20228d356d375fc918
Squaredcandy <squaredcandy@gmail.com>
2024-04-16 22:34:49 +1000

    Adding Elevation Tokens

---
+++ lunar/src/main/java/com/squaredcandy/lunar/theme/LunarElevationTokens.kt
@@ -0,0 +1,12 @@
+package com.squaredcandy.lunar.theme
+
+import androidx.compose.ui.unit.dp
+
+object LunarElevationTokens {
+    val Level0 = 0.0.dp
+    val Level1 = 1.0.dp
+    val Level2 = 3.0.dp
+    val Level3 = 6.0.dp
+    val Level4 = 8.0.dp
+    val Level5 = 12.0.dp
+}
